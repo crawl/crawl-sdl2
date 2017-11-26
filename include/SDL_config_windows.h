fc9538c1287e6c73c120a56febaabfa6b59c19ee
gammafunk <gammafunk@gmail.com>
2017-11-25 23:22:49 -0600

    Disable SDL audio WASAPI driver
    Otherwise I get a link error when cross-compiling using mingw.

--- include/SDL_config_windows.h
+++ include/SDL_config_windows.h
@@ -165,7 +165,7 @@ typedef unsigned int uintptr_t;
 #endif
 
 /* Enable various audio drivers */
-#define SDL_AUDIO_DRIVER_WASAPI 1
+#define SDL_AUDIO_DRIVER_WASAPI 0
 #define SDL_AUDIO_DRIVER_DSOUND 1
 #define SDL_AUDIO_DRIVER_XAUDIO2    0
 #define SDL_AUDIO_DRIVER_WINMM  1
