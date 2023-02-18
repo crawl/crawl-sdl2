f0532b9032f54319444a1366047905cbfbcb1318
Kyle Rawlins <rawlins@gmail.com>
2023-02-18 07:57:47 -0500

    fix: merge upstream changes to capslock handling
    This is aimed to prevent MacOS builds (post catalina) from asking for
    input monitoring permission; it's unnecessarily alarming and isn't
    needed. Plus, as far as I can tell the permissions request resulted from
    a misimplementation of capslock handling in the first place.
    
    This change was originall made in
    https://github.com/libsdl-org/SDL/commit/57e08c27ef4a54e48c20c6bfd7c841216a6b70b5,
    but I've taken up-to-date SDL2 code, which seems to have some other
    tweaks.

--- src/video/cocoa/SDL_cocoawindow.m
+++ src/video/cocoa/SDL_cocoawindow.m
@@ -835,13 +835,29 @@ SetWindowStyle(SDL_Window * window, NSUInteger style)
 }
 
 
-/* We'll respond to key events by doing nothing so we don't beep.
+/* We'll respond to key events by mostly doing nothing so we don't beep.
  * We could handle key messages here, but we lose some in the NSApp dispatch,
  * where they get converted to action messages, etc.
  */
 - (void)flagsChanged:(NSEvent *)theEvent
 {
     /*Cocoa_HandleKeyEvent(SDL_GetVideoDevice(), theEvent);*/
+
+    /* Catch capslock in here as a special case:
+       https://developer.apple.com/library/archive/qa/qa1519/_index.html
+       Note that technote's check of keyCode doesn't work. At least on the
+       10.15 beta, capslock comes through here as keycode 255, but it's safe
+       to send duplicate key events; SDL filters them out quickly in
+       SDL_SendKeyboardKey(). */
+
+    /* Also note that SDL_SendKeyboardKey expects all capslock events to be
+       keypresses; it won't toggle the mod state if you send a keyrelease.  */
+    const SDL_bool osenabled = ([theEvent modifierFlags] & NSEventModifierFlagCapsLock) ? SDL_TRUE : SDL_FALSE;
+    const SDL_bool sdlenabled = (SDL_GetModState() & KMOD_CAPS) ? SDL_TRUE : SDL_FALSE;
+    if (osenabled ^ sdlenabled) {
+        SDL_SendKeyboardKey(SDL_PRESSED, SDL_SCANCODE_CAPSLOCK);
+        SDL_SendKeyboardKey(SDL_RELEASED, SDL_SCANCODE_CAPSLOCK);
+    }
 }
 - (void)keyDown:(NSEvent *)theEvent
 {
